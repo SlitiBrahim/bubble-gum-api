eba58cdbd265431cd1cd9dcbbdfcc0780d346718
SLITI Brahim <bsliti@wanadev.fr>
2018-10-04 16:14:09 +0200

    Handling no route found response

--- app.js
+++ app.js
@@ -6,4 +6,12 @@ const userController = require('./components/user/userController');
 
 app.use('/users', userController);
 
+// In case any controller was matched throw an error
+app.use((req, res) => {
+    // return json error
+    res.status(404).json({
+        message: "No route found for this resource or the HTTp verb is invalid, please refer to the documentation."
+    });
+});
+
 module.exports = app;
