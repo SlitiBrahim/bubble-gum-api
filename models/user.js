1e50c1170972bda38d8648bec5d3f93fa361c231
SLITI Brahim <brahim.sliti@supinfo.com>
2018-10-15 07:02:01 +0200

    Implementing User model

---
+++ models/user.js
@@ -0,0 +1,32 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const user = sequelize.define('user', {
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
+    userName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    deletedAt: {
+      type: DataTypes.DATE
+    },
+    bannedAt: {
+      type: DataTypes.DATE
+    },
+    birthdayDate: {
+      type: DataTypes.DATE
+    }
+  }, {});
+  user.associate = function(models) {
+    // associations can be defined here
+  };
+  return user;
+};
