81f6786055c6689afbd76e7eb49afbdd7efbed91
SLITI Brahim <brahim.sliti@supinfo.com>
2018-10-18 20:59:15 +0200

    Making User username property as lowercase

--- models/user.js
+++ models/user.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
         }
       }
     },
-    userName: {
+    username: {
       type: DataTypes.STRING,
       allowNull: false,
       unique: true,
